28084ccf6a853adfb5be1fdda4d4c3bc011d0f58
yhu <hooby@sg-ai.com>
2019-04-22 16:53:49 +0800

    change to run with main

--- debug.py
+++ debug.py
@@ -1,50 +1,34 @@
 from generate_mask_geojson import generate_mask
 import os
 from skimage import io
-# image_size = (2048, 2048)
+image_size = (2048, 2048)
+files_proc = ['datasets/anet_png/test/w11_bac_tubggcp3_4430_p12_/annotation.json']
 
-# train_list = ["w11_bac_bora_4437_p02_",
-#               "w11_bac_bora_4437_p13_",
-#               "w11_bac_bora_4437_p30_",
-#               "w11_bac_tubggcp3_4430_p04_",
-#               "w13_bac_usf1_7394_p02_",
-#               "w1_bac_kif1c_6512_p02_"]
+generate_mask(files_proc=files_proc, image_size=image_size)
+
+# import os
+# import shutil
+# from PIL import Image
 #
-# files_proc = ["datasets/anet/train/w1_bac_kif1c_6512_p02_/annotation.json",
-#               "datasets/anet/train/w11_bac_bora_4437_p13_/annotation.json",
-#               "datasets/anet/train/w11_bac_bora_4437_p30_/annotation.json",
-#               "datasets/anet/train/w11_bac_tubggcp3_4430_p04_/annotation.json",
-#               "datasets/anet/train/w13_bac_usf1_7394_p02_/annotation.json",]
-
-# generate_mask(files_proc=files_proc, image_size=image_size)
-
-# files_proc = ["datasets/anet/train/w11_bac_bora_4437_p02_/annotation__jeojson_fill.json"]
-# files_proc = ["datasets/example/train/z010/annotation__jeojson_fill.json"]
-
-
-import os
-import shutil
-from PIL import Image
-
-sample_dir = "datasets/anet/"
-# train_dir = "datasets/anet/train"
-train_dir = "datasets/anet/valid"
-
-sample_list = os.listdir(train_dir)
-for sample in sample_list:
-    tiff_list = os.listdir(os.path.join(train_dir, sample))
-    for file in tiff_list:
-        if file.endswith(".tif"):
-            print("sample:", sample)
-            print("file:", file)
-            tif = Image.open(os.path.join(train_dir, sample, file)).convert(mode='I')
-            # print(type(tif))
-            # print(tif.size)
-            save_dir = os.path.dirname(os.path.join(train_dir, sample, file).replace("anet", "anet_png"))
-
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir)
-            save_file = os.path.join(train_dir, sample, file).replace("anet", "anet_png").replace(".tif", ".png")
-            print("save_dir:", save_dir)
-            print("save_file:", save_file)
-            tif.save(save_file)
+# sample_dir = "datasets/anet/"
+# # train_dir = "datasets/anet/train"
+# train_dir = "datasets/anet/valid"
+#
+# sample_list = os.listdir(train_dir)
+# for sample in sample_list:
+#     tiff_list = os.listdir(os.path.join(train_dir, sample))
+#     for file in tiff_list:
+#         if file.endswith(".tif"):
+#             print("sample:", sample)
+#             print("file:", file)
+#             tif = Image.open(os.path.join(train_dir, sample, file)).convert(mode='I')
+#             # print(type(tif))
+#             # print(tif.size)
+#             save_dir = os.path.dirname(os.path.join(train_dir, sample, file).replace("anet", "anet_png"))
+#
+#             if not os.path.exists(save_dir):
+#                 os.makedirs(save_dir)
+#             save_file = os.path.join(train_dir, sample, file).replace("anet", "anet_png").replace(".tif", ".png")
+#             print("save_dir:", save_dir)
+#             print("save_file:", save_file)
+#             tif.save(save_file)
--- dev__postProcess.py
+++ dev__postProcess.py
@@ -1,4 +1,3 @@
-
 import importlib
 import sys
 import os
@@ -8,28 +7,6 @@ import segmentationUtils
 importlib.reload(segmentationUtils)
 
 
-# outputs_dir = 'datasets/anet_png/train/w11_bac_bora_4437_p02_/'
-# # outputs_dir = 'datasets/example/train/z010/'
-# simplify_tol = 0  # Tolerance for polygon simplification with shapely (0 to not simplify)
-#
-# if os.path.exists(outputs_dir):
-#     for file in [f for f in os.listdir(outputs_dir) if 'MASK' in f][:1]:
-#         file = os.path.join(outputs_dir, file)
-#         # Read png with input and output
-#         mask = io.imread(file)
-#         print("file:",file)
-#         print("mask.shape:", mask.shape)
-#         # Call function to transform segmentation masks into (geojson) polygons
-#         segmentationUtils.masks_to_polygon(mask,
-#                                            simplify_tol=simplify_tol,
-#                                            plot_simplify=False,
-#                                            save_name=file
-#                                            .replace('.png', '.json')
-#                                            .replace('MASK', 'jeojson'))
-#
-#         print("segmentationUtils convert success")
-
-
 def masks_to_annotation(filepath, save_name, simplify_tol=0, plot_simplify=False):
     mask = io.imread(filepath)
     contours, feature_collection = segmentationUtils.masks_to_polygon(mask,
@@ -37,3 +14,26 @@ def masks_to_annotation(filepath, save_name, simplify_tol=0, plot_simplify=False
                                                                       plot_simplify=plot_simplify,
                                                                       save_name=save_name)
     return feature_collection
+
+
+if __name__ == '__main__':
+    outputs_dir = 'datasets/anet_png/train/w11_bac_bora_4437_p02_/'
+    # outputs_dir = 'datasets/example/train/z010/'
+    simplify_tol = 0  # Tolerance for polygon simplification with shapely (0 to not simplify)
+
+    if os.path.exists(outputs_dir):
+        for file in [f for f in os.listdir(outputs_dir) if 'MASK' in f][:1]:
+            file = os.path.join(outputs_dir, file)
+            # Read png with input and output
+            mask = io.imread(file)
+            print("file:",file)
+            print("mask.shape:", mask.shape)
+            # Call function to transform segmentation masks into (geojson) polygons
+            segmentationUtils.masks_to_polygon(mask,
+                                               simplify_tol=simplify_tol,
+                                               plot_simplify=False,
+                                               save_name=file
+                                               .replace('.png', '.json')
+                                               .replace('MASK', 'jeojson'))
+
+            print("segmentationUtils convert success")
